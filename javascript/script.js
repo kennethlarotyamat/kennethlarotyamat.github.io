deb0ce9c4aed377766ec67fd2a45595e207daf66
Kenneth Larot Yamat <info@chestnutandhazel.com>
2023-09-23 06:41:45 -0700

    starting to work 09232023 06411111 00
    starting to work 09232023 06411111 00

--- javascript/script.js
+++ javascript/script.js
@@ -8,9 +8,11 @@ $(function(){
 });  
 
 
-let body = document.querySelector("body");
-
-let footer = document.querySelector("footer");
+let body = document.querySelector(" body");
+let footer = document.querySelector(" footer");
+let main = document.querySelector(" main");
+let aside = document.querySelector(" aside");
+let asideTwo = document.querySelector(".aside-two");
 
 let modeToggle = document.getElementById('mode-toggle');
 let modeStatus = document.querySelector('.mode-status');
@@ -18,6 +20,10 @@ let modeStatus = document.querySelector('.mode-status');
 function toggleMode() {
   body.classList.toggle('dark-mode');
   footer.classList.toggle('dark-mode');
+  main.classList.toggle('dark-mode');
+  aside.classList.toggle('dark-mode');
+  asideTwo.classList.toggle('dark-mode');
+
 
   let modeMessage = body.classList.contains('dark-mode') ?
     'Grayscale' 
