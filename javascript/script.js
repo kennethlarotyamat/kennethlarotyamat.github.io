57387c5dd3284392ea21e4fde852a0ea3ec69d0b
Kenneth Larot Yamat <info@chestnutandhazel.com>
2023-09-22 18:33:38 -0700

    09222023 063301
    this better work

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,20 @@ $(function(){
     });
   });
 }); 
+
+
+const body = document.querySelector('body');
+const modeToggle = document.getElementById('mode-toggle');
+const modeStatus = document.querySelector('.mode-status');
+
+function toggleMode() {
+  body.classList.toggle('dark-mode');
+
+  const modeMessage = body.classList.contains('dark-mode') ?
+    'Grayscale' 
+    : "Color"
+
+  modeStatus.innerText = modeMessage;
+}
+
+modeToggle.addEventListener('click', toggleMode);
