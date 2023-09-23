368412d0e492510565f0db4a2bb42817af2388a2
Kenneth Larot Yamat <info@chestnutandhazel.com>
2023-09-23 07:45:42 -0700

    1
    1

--- javascript/script.js
+++ javascript/script.js
@@ -12,41 +12,19 @@ let body = document.querySelector(" body");
 let footer = document.querySelector(" footer");
 let main = document.querySelector(" main");
 let aside = document.querySelector(" aside");
-let asideTwo = document.querySelector("aside-two");
-let asideTwoColorToggler00 = document.querySelector("#bodyid1651231");
-let asideTwoColorToggler01 = document.querySelector("#subheadercenterheadertextbutton");
-let asideTwoColorToggler02 = document.querySelector(".header");
-let asideTwoColorToggler03 = document.querySelector(".contentouter");
-let asideTwoColorToggler04 = document.querySelector(".sidebarouter");
-let asideTwoColorToggler05 = document.querySelector(".containeroverouter");
-let asideTwoColorToggler06 = document.querySelector(".aside-one");
-let asideTwoColorToggler07 = document.querySelector(".aside-two");
-let asideTwoColorToggler08 = document.querySelector("body");
-let asideTwoColorToggler09 = document.querySelector(".footer");
-let asideTwoColorToggler10 = document.querySelector("footer");
-let asideTwoColorToggler11 = document.querySelector("backgroundcolortoggler");
+let asideTwo = document.querySelector(".aside-two");
 
 let modeToggle = document.getElementById('mode-toggle');
 let modeStatus = document.querySelector('.mode-status');
 
-function toggleMode() {
+function toggleMode() {  
   body.classList.toggle('dark-mode');
   footer.classList.toggle('dark-mode');
   main.classList.toggle('dark-mode');
   aside.classList.toggle('dark-mode');
   asideTwo.classList.toggle('dark-mode');
-  asideTwoColorToggler00.classList.toggle('dark-mode');
-  asideTwoColorToggler01.classList.toggle('dark-mode');
-  asideTwoColorToggler02.classList.toggle('dark-mode');
-  asideTwoColorToggler03.classList.toggle('dark-mode');
-  asideTwoColorToggler04.classList.toggle('dark-mode');
-  asideTwoColorToggler05.classList.toggle('dark-mode');
-  asideTwoColorToggler06.classList.toggle('dark-mode');
-  asideTwoColorToggler07.classList.toggle('dark-mode');
-  asideTwoColorToggler08.classList.toggle('dark-mode');
-  asideTwoColorToggler09.classList.toggle('dark-mode');
-  asideTwoColorToggler10.classList.toggle('dark-mode');
-  asideTwoColorToggler11.classList.toggle('dark-mode');
+
+
 
 
   let modeMessage = body.classList.contains('dark-mode') ?
