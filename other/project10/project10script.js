607247adf36e9946a15d14b5b6959b6cf9a4c4b8
Kenneth Larot Yamat <info@chestnutandhazel.com>
2023-09-30 04:57:59 -0700

    09302023 0457 00
    09302023 0457 00

---
+++ other/project10/project10script.js
@@ -0,0 +1,65 @@
+// 09 23 2023 09 40 00
+
+
+
+
+
+
+
+$(function(){
+  $("body").on('hidden.bs.modal', function (e) {
+    var $iframes = $(e.target).find("iframe");
+    $iframes.each(function(index, iframe){
+      $(iframe).attr("src", $(iframe).attr("src"));
+    });
+  });
+});  
+
+// can create a new claass and use query selector all in the fucntion use a for each or for each loop use for each and selector all function create branch to try alternative approach
+ 
+let bodyA = document.querySelector(" .bodya");
+let footer = document.querySelector(" footer");
+let main = document.querySelector(" .main");
+let aside = document.querySelector(" aside");
+let asideTwo = document.querySelector(".aside-two");
+let subheadercenterheadertextbutton = document.querySelector(".subheadercenterheadertextbutton");
+let header = document.querySelector(".header");
+let contentouter = document.querySelector(".contentouter");
+let sidebarouter = document.querySelector(".sidebarouter");
+let bootstrapbuttonone = document.querySelector(".bootstrapbuttonone");
+let bootstrapbuttontwo = document.querySelector(".bootstrapbuttontwo");
+let bootstrapbuttonthree = document.querySelector(".bootstrapbuttonthree");
+let bootstrapbuttonfour = document.querySelector(".bootstrapbuttonfour");
+let bootstrapbuttonfive = document.querySelector(".bootstrapbuttonfive");
+
+let modeToggle = document.getElementById('mode-toggle');
+let modeStatus = document.querySelector('.mode-status');
+ 
+function toggleMode() {  
+  bodyA.classList.toggle('dark-modefields');
+  footer.classList.toggle('dark-modefields');
+  main.classList.toggle('dark-modefields');
+  aside.classList.toggle('dark-modefields');
+  asideTwo.classList.toggle('dark-modefields');
+  subheadercenterheadertextbutton.classList.toggle('dark-modefields');
+  header.classList.toggle('dark-modefields');
+  contentouter.classList.toggle('dark-mode');
+  sidebarouter.classList.toggle('dark-modefields');
+  bootstrapbuttonone.classList.toggle('dark-modebsbuttons');
+  bootstrapbuttontwo.classList.toggle('dark-modebsbuttons');
+  bootstrapbuttonthree.classList.toggle('dark-modebsbuttons');
+  bootstrapbuttonfour.classList.toggle('dark-modebsbuttons');
+  bootstrapbuttonfive.classList.toggle('dark-modebsbuttons');
+  
+
+
+
+
+  let modeMessage = bodyA.classList.contains('dark-modefields') ?
+    'Grayscale' 
+    : "Color";
+
+  modeStatus.innerText =  modeMessage;
+}
+
+modeToggle.addEventListener('click', toggleMode);
