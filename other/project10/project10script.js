fef27026cb8b8ecfbe3ea8a83ccab3866775b48a
Kenneth Larot Yamat <info@chestnutandhazel.com>
2023-09-30 06:56:32 -0700

    09302023 0656 00
    09302023 0656 00

--- other/project10/project10script.js
+++ other/project10/project10script.js
@@ -63,3 +63,49 @@ function toggleMode() {
 }
 
 modeToggle.addEventListener('click', toggleMode);
+
+
+const images = document.querySelectorAll('#sliderproject10 img');
+const previousImage = document.getElementById("prevproject10");
+const nextImage = document.getElementById("nextproject10");
+
+let currentIndex = 0;
+
+function reset() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('active');
+  }
+}
+
+function initializeSlider() {
+  reset();
+  images[currentIndex].classList.add('active');
+}
+
+function slideLeft() {
+  reset();
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+function slideRight() {
+  reset();
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+initializeSlider();
+
+previousImage.addEventListener('click', function() {
+  slideLeft();
+});
+
+nextImage.addEventListener('click', function() {
+  slideRight();
+});
