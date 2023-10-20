b2d5d0e56f31902eb541f9f699462da11fffb82d
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-10-20 02:57:10 -0700

    10202023 0257

--- other/OHLCEstimator/ohlcestimator.js
+++ other/OHLCEstimator/ohlcestimator.js
@@ -17,7 +17,7 @@ const svg = d3.select('#body')
 
 
 // STEP 2: Create an array of data
-const data = [ ,  ,  ,  , 200];
+const data = [ ,  ,  ,20  , 200, 50];
 
 
 // STEP 3: Create a rectangle for each data point
@@ -39,27 +39,32 @@ svg.selectAll('rect') // Select all existing rect elements
   // STEP 5: Set the fill color of each rectangle to blue using the style method
   .style('fill', 'blue');
 
-  const dataOpen = [ ,  ,  100,  , , ];
 
 
-// STEP 3: Create a rectangle for each data point
-svg.selectAll('rect') // Select all existing rect elements
-  // Bind the data array to the selection
-  .data(dataOpen)
-  // Create a placeholder for each data point that doesn't have a corresponding rect element yet
-  .enter()
-  // Append a rect element for each placeholder
-  .append('rect')
-  // Set the width of each rect element to 50
-  .attr('width', 10)
-  // Set the height of each rect element to the corresponding data value
-  .attr('height', d => d)
-  // STEP 4: Position each rectangle along the x-axis based on its index in the data array
-  .attr('x', (d, i) => i * 60)
-  // Invert the y-axis and set the y-position of each rect element based on its data value
-  .attr('y', d => 300 - d)
-  // STEP 5: Set the fill color of each rectangle to blue using the style method
-  .style('fill', 'blue');
+
+
+
+//   const dataOpen = [ ,  ,  100,  , , ];
+
+
+// // STEP 3: Create a rectangle for each data point
+// svg.selectAll('rect') // Select all existing rect elements
+//   // Bind the data array to the selection
+//   .data(dataOpen)
+//   // Create a placeholder for each data point that doesn't have a corresponding rect element yet
+//   .enter()
+//   // Append a rect element for each placeholder
+//   .append('rect')
+//   // Set the width of each rect element to 50
+//   .attr('width', 10)
+//   // Set the height of each rect element to the corresponding data value
+//   .attr('height', d => d)
+//   // STEP 4: Position each rectangle along the x-axis based on its index in the data array
+//   .attr('x', (d, i) => i * 60)
+//   // Invert the y-axis and set the y-position of each rect element based on its data value
+//   .attr('y', d => 300 - d)
+//   // STEP 5: Set the fill color of each rectangle to blue using the style method
+//   .style('fill', 'blue');
 
 
 const body = document.querySelector('body');
