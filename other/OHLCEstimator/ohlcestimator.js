f23bed1e6dca257ab6fac8f0f8caa54be6827ea2
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-10-20 06:35:07 -0700

    10202023 0635

--- other/OHLCEstimator/ohlcestimator.js
+++ other/OHLCEstimator/ohlcestimator.js
@@ -2,41 +2,32 @@
 d3.select('#body')
   .append('div')
   .attr('id', 'myDiv')
-
-
 d3.select('#myDiv')
   .append('h1')
   .text('OHLC Estimator')
-
-
 const svg = d3.select('#body')
   .append('svg')
-  .style('border', '2px solid black')
   .attr('width', 500)
   .attr('height', 500);
 
 
-const data = [  ,2,50,2,   ];
 
 
 
-svg.selectAll('rect')
-  
-  .data(data)
-  
-  .enter()
- 
-  .append('rect')
-  
-  .attr('width', 2)
-  
-  .attr('height', d => d)
-  
-  .attr('x', (d, i) => i * 125)
-  
-  .attr('y', d => 300 - d)
-  
-  .style('fill', 'blue');
+
+const data = [  ,   ,300,2,   ];
+const dataOpen = [  , 2, , ,   ];
+const dataClose = [  ,  , ,2 ,   ];
+svg.selectAll('rect')  
+  .data(data)  
+  .enter() 
+  .append('rect')  
+  .attr('width', 100)  
+  .attr('height', d => d)  
+  .attr('x', (d, i) => i * 100)  
+  .attr('y', d =>  450 - d)  
+  .style('fill', 'blue')  
+
 
 
 
