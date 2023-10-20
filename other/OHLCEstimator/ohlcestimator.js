43aadb85b469f72e462a05a63b47ff6c00ee52e6
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-10-20 04:36:37 -0700

    10202023 0436

--- other/OHLCEstimator/ohlcestimator.js
+++ other/OHLCEstimator/ohlcestimator.js
@@ -15,29 +15,6 @@ const svg = d3.select('#body')
   .attr('width', 500)
   .attr('height', 500);
 
-  const dataOpen = [ , , 10 , , , ,  ];
-
-
-  // STEP 3: Create a rectangle for each data point
-  svg.selectAll('rectOpen') // Select all existing rect elements
-    // Bind the data array to the selection
-    .data(dataOpen)
-    // Create a placeholder for each data point that doesn't have a corresponding rect element yet
-    .enter()
-    // Append a rect element for each placeholder
-    .append('rectOpen')
-    // Set the width of each rect element to 50
-    .attr('width', 2)
-    // Set the height of each rect element to the corresponding data value
-    .attr('height', d => d)
-    // STEP 4: Position each rectangle along the x-axis based on its index in the data array
-    .attr('x', (d, i) => i * 60)
-    // Invert the y-axis and set the y-position of each rect element based on its data value
-    .attr('y', d => 300 - d)
-    // STEP 5: Set the fill color of each rectangle to blue using the style method
-    .style('fill', 'blue');
-
- 
 // STEP 2: Create an array of data
 const data = [ , , , 50 , , ,  ];
 
@@ -61,58 +38,9 @@ svg.selectAll('rect') // Select all existing rect elements
   // STEP 5: Set the fill color of each rectangle to blue using the style method
   .style('fill', 'blue');
 
-  const dataClose = [ , , , , 40 , ,  ];
 
 
-  // STEP 3: Create a rectangle for each data point
-  svg.selectAll('rectClose') // Select all existing rect elements
-    // Bind the data array to the selection
-    .data(dataClose)
-    // Create a placeholder for each data point that doesn't have a corresponding rect element yet
-    .enter()
-    // Append a rect element for each placeholder
-    .append('rectClose')
-    // Set the width of each rect element to 50
-    .attr('width', 2)
-    // Set the height of each rect element to the corresponding data value
-    .attr('height', d => d)
-    // STEP 4: Position each rectangle along the x-axis based on its index in the data array
-    .attr('x', (d, i) => i * 60)
-    // Invert the y-axis and set the y-position of each rect element based on its data value
-    .attr('y', d => 300 - d)
-    // STEP 5: Set the fill color of each rectangle to blue using the style method
-    .style('fill', 'blue');
-
-
-
-
-
-
-
-
-//   const dataOpen = [ ,  ,  100,  , , ];
-
-
-// // STEP 3: Create a rectangle for each data point
-// svg.selectAll('rect') // Select all existing rect elements
-//   // Bind the data array to the selection
-//   .data(dataOpen)
-//   // Create a placeholder for each data point that doesn't have a corresponding rect element yet
-//   .enter()
-//   // Append a rect element for each placeholder
-//   .append('rect')
-//   // Set the width of each rect element to 50
-//   .attr('width', 10)
-//   // Set the height of each rect element to the corresponding data value
-//   .attr('height', d => d)
-//   // STEP 4: Position each rectangle along the x-axis based on its index in the data array
-//   .attr('x', (d, i) => i * 60)
-//   // Invert the y-axis and set the y-position of each rect element based on its data value
-//   .attr('y', d => 300 - d)
-//   // STEP 5: Set the fill color of each rectangle to blue using the style method
-//   .style('fill', 'blue');
-
-
+ 
 const body = document.querySelector('body');
 const modeToggle = document.getElementById('mode-toggle');
 const modeStatus = document.querySelector('.mode-status');
