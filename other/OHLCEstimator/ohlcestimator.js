d8f3996fbbc4fa6be099d4a9c041e4b4c964f643
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-10-20 02:52:12 -0700

    10202023 0252

--- other/OHLCEstimator/ohlcestimator.js
+++ other/OHLCEstimator/ohlcestimator.js
@@ -11,13 +11,13 @@ d3.select('#myDiv')
 // STEP 1: Create an SVG element with a width of 500, a height of 300, and a solid blue border 2 pixels wide
 const svg = d3.select('#body')
   .append('svg')
-  .style('border', '2px solid blue')
+  .style('border', '2px solid black')
   .attr('width', 500)
   .attr('height', 500);
 
 
 // STEP 2: Create an array of data
-const data = [10, 20, 30, 40, 50];
+const data = [ ,  ,  ,  , 200];
 
 
 // STEP 3: Create a rectangle for each data point
@@ -29,7 +29,29 @@ svg.selectAll('rect') // Select all existing rect elements
   // Append a rect element for each placeholder
   .append('rect')
   // Set the width of each rect element to 50
-  .attr('width', 50)
+  .attr('width', 2)
+  // Set the height of each rect element to the corresponding data value
+  .attr('height', d => d)
+  // STEP 4: Position each rectangle along the x-axis based on its index in the data array
+  .attr('x', (d, i) => i * 60)
+  // Invert the y-axis and set the y-position of each rect element based on its data value
+  .attr('y', d => 300 - d)
+  // STEP 5: Set the fill color of each rectangle to blue using the style method
+  .style('fill', 'blue');
+
+  const dataOpen = [ ,  ,  100,  , , ];
+
+
+// STEP 3: Create a rectangle for each data point
+svg.selectAll('rect') // Select all existing rect elements
+  // Bind the data array to the selection
+  .data(dataOpen)
+  // Create a placeholder for each data point that doesn't have a corresponding rect element yet
+  .enter()
+  // Append a rect element for each placeholder
+  .append('rect')
+  // Set the width of each rect element to 50
+  .attr('width', 10)
   // Set the height of each rect element to the corresponding data value
   .attr('height', d => d)
   // STEP 4: Position each rectangle along the x-axis based on its index in the data array
