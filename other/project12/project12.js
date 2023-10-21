1259e376b36bfe3fd5ce577d556a1091e71c741b
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-10-21 09:55:06 -0700

    10212023 0954 // That was a total nightmare. What I did was I decided to use the outline of my landing page as a kind of outline for Project 12, but // so I deleted a whole bunch of stuff, buttons and DIV sections and whatever from my HTML Landing page, and used the shell here for // the project 12 quiz project. I don't even want to think about how much time I spent just trying to get this button to work again. // but basically, those items I deleted were part of the series of transactions that javascript performs to come up with the message: color grayscale, // one those html elemnts were removed, then javascript couldn't perform the final transaction that allows the message change.
    // Store all form elements in an array by spreading the elements property of the form
      // Create function to check if all form elements are valid

--- other/project12/project12.js
+++ other/project12/project12.js
@@ -57,14 +57,7 @@ modeToggle.addEventListener('click', toggleMode);
 
 // End Color - Grayscale Toggler
 
-// That was a total nightmare. What I did was I decided to use the outline of my landing page as a kind of outline for Project 12, but
-// so I deleted a whole bunch of stuff, buttons and DIV sections and whatever from my HTML Landing page, and used the shell here for
-// the project 12 quiz project. I don't even want to think about how much time I spent just trying to get this button to work again.
-// but basically, those items I deleted were part of the series of transactions that javascript performs to come up with the message: color grayscale,
-// one those html elemnts were removed, then javascript couldn't perform the final transaction that allows the message change.
 
-// Store all form elements in an array by spreading the elements property of the form
-  // Create function to check if all form elements are valid
 
 
   const form = document.getElementById('exampleForm',)
