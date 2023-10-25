16fd119dc5f4153e0e8570af401667fcaad1dc02
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-10-24 18:20:54 -0700

    10242023 1820

--- other/project12/project12.js
+++ other/project12/project12.js
@@ -151,3 +151,27 @@ $(document).ready(function() {
 });
 
 
+
+// // SELECTING CORRECT ANSWER
+// $('#q1__answer-selection-3').on('click', function() {
+//   $('.feedback').text('Correct Answer!').addClass("correct-answer");
+//   $('.answer').attr('disabled',true).removeClass("hover-effect").off('mouseenter').off('mouseleave');
+//   $('.next-question').attr('disabled', false).on("click", function(){
+//     alert("Thank You! Play Again");
+//     location.reload();
+//   });
+//   $(this).addClass("correct-answer");
+// });
+
+// //SELECTING WRONG ANSWERS
+// $('#q1__answer-selection-1,#q1__answer-selection-2, #q1__answer-selection-4').on('click', function() {
+//   $('.feedback').text('Incorrect Answer!').addClass("incorrect-answer");
+//   $('.answer').attr('disabled',true).removeClass("hover-effect").off('mouseenter').off('mouseleave');
+//   $('.next-question').attr('disabled', false).text("Try Again").on("click", function(){
+//     location.reload();
+//   });
+//   $(this).addClass("incorrect-answer");
+// });
+
+// resource: https://github.com/Jamiecodio/fitness-quiz/blob/master/script.js
+
