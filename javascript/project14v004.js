26ffefb2d2f309a1ec24f9e61129913f7a2a9b02
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-04 13:28:12 -0700

    1

---
+++ javascript/project14v004.js
@@ -0,0 +1,11 @@
+//              guess what? This is a nightmare!
+//             [Resources]      [https://stackoverflow.com/questions/77422567/bootstrap-carousel-issues-bootstrap-version-5]
+//                              [https://kennethlarotyamat.github.io/html/project14.html]
+//                              [https://stackoverflow.com/questions/36972571/bootstrap-carousel-issue]
+//                              sometimes I can't even understand what I'm reading.
+//                              [https://stackoverflow.com/questions/36972571/bootstrap-carousel-issue]
+//                              [https://stackoverflow.com/questions/67120095/bootstrap-5-carousel-with-multiple-items]
+//                              [https://kennethlarotyamat.github.io/html/project14.html]
+//                              [https://kennethlarotyamat.github.io/html/project14.html]
+//                              [https://strategywiki.org/wiki/SimCity_4/Other_Transportation]
+//                              [https://www.chestnutandhazel.com/]
