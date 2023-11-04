d2ea23527d00ad34a78d37e472e5dab19a77aa06
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-04 13:31:44 -0700

    1

--- javascript/project14v004.js
+++ javascript/project14v004.js
@@ -9,3 +9,48 @@
 //                              [https://kennethlarotyamat.github.io/html/project14.html]
 //                              [https://strategywiki.org/wiki/SimCity_4/Other_Transportation]
 //                              [https://www.chestnutandhazel.com/]
+
+const images = document.querySelectorAll('#sliderproject10 iframe');
+const previousImage = document.getElementById("prevproject10");
+const nextImage = document.getElementById("nextproject10");
+
+let currentIndex = 0;
+ 
+function reset() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('active');
+  }
+}
+
+function initializeSlider() {
+  reset();
+  images[currentIndex].classList.add('active');
+} 
+ 
+function slideLeft() {
+  reset();
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  images[currentIndex].classList.add('active');
+}
+ 
+function slideRight() {
+  reset();
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+initializeSlider();
+
+previousImage.addEventListener('click', function() {
+  slideLeft();
+});
+
+nextImage.addEventListener('click', function() {
+  slideRight();
+});
