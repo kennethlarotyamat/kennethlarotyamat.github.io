234f7b4092f1f2a4be313eb7aaef67704cf6532f
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-04 08:26:18 -0700

    1guess what? This is a nightmare!

--- javascript/project14v002.js
+++ javascript/project14v002.js
@@ -1,1 +1,3 @@
-//  guess what? This is a nightmare!
+//              guess what? This is a nightmare!
+//             [Resources]      [https://stackoverflow.com/questions/77422567/bootstrap-carousel-issues-bootstrap-version-5]
+//                              [https://kennethlarotyamat.github.io/html/project14.html]
