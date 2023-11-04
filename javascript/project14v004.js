ff1a29d54462c03c26ed0bd8f46ff4dd13129494
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-04 13:58:12 -0700

    1

--- javascript/project14v004.js
+++ javascript/project14v004.js
@@ -1,16 +1,16 @@
-//              guess what? This is a nightmare!
-//             [Resources]      [https://stackoverflow.com/questions/77422567/bootstrap-carousel-issues-bootstrap-version-5]
-//                              [https://kennethlarotyamat.github.io/html/project14.html]
-//                              [https://stackoverflow.com/questions/36972571/bootstrap-carousel-issue]
-//                              sometimes I can't even understand what I'm reading.
-//                              [https://stackoverflow.com/questions/36972571/bootstrap-carousel-issue]
-//                              [https://stackoverflow.com/questions/67120095/bootstrap-5-carousel-with-multiple-items]
-//                              [https://kennethlarotyamat.github.io/html/project14.html]
-//                              [https://kennethlarotyamat.github.io/html/project14.html]
-//                              [https://strategywiki.org/wiki/SimCity_4/Other_Transportation]
-//                              [https://www.chestnutandhazel.com/]
+
+// window.onload = function () {
+//     var e = document.getElementById("db_info");
+//     e.innerHTML='Found you';
+// };
+
+
 
 const previousImage = document.getElementById("prevproject10");
+window.onload = function () {
+    const previousImage = document.getElementById("prevproject10");
+    
+};
 
 const images = document.querySelectorAll('#sliderproject10 iframe');
 
@@ -58,3 +58,16 @@ previousImage.addEventListener('click', function() {
 nextImage.addEventListener('click', function() {
   slideRight();
 });
+
+//              guess what? This is a nightmare!
+//             [Resources]      [https://stackoverflow.com/questions/77422567/bootstrap-carousel-issues-bootstrap-version-5]
+//                              [https://kennethlarotyamat.github.io/html/project14.html]
+//                              [https://stackoverflow.com/questions/36972571/bootstrap-carousel-issue]
+//                              sometimes I can't even understand what I'm reading.
+//                              [https://stackoverflow.com/questions/36972571/bootstrap-carousel-issue]
+//                              [https://stackoverflow.com/questions/67120095/bootstrap-5-carousel-with-multiple-items]
+//                              [https://kennethlarotyamat.github.io/html/project14.html]
+//                              [https://kennethlarotyamat.github.io/html/project14.html]
+//                              [https://strategywiki.org/wiki/SimCity_4/Other_Transportation]
+//                              [https://www.chestnutandhazel.com/]
+
