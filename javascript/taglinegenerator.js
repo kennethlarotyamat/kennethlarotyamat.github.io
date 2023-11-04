122eb35141f998265304fb5b1ec864bb9c9dc7d8
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-04 09:38:10 -0700

    1

---
+++ javascript/taglinegenerator.js
@@ -0,0 +1,16 @@
+// const fs = require('fs');
+
+// 1. Read the JSON file
+// const rawData = fs.readFileSync('C:\Users\KLYam\OneDrive\Documents\GitHub\kennethlarotyamat.github.io\javascript\project14v003.json');
+// const data = JSON.parse(rawData);
+
+// 3. Retrieve the necessary data
+// const desiredData = data.tagline; 
+// Replace 'key' with the specific key you want
+
+// 4. Generate your statement
+// const statement = `This is based on the data from the JSON file: ${desiredData}`;
+
+// console.log(statement); 
+
+// i don't understand these instructions.
