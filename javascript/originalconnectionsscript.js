b7ad010f49268fb4c04370175504825c4ae85635
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-06 08:07:29 -0800

    1

--- javascript/originalconnectionsscript.js
+++ javascript/originalconnectionsscript.js
@@ -6,3 +6,6 @@ $(function(){
     });
   });
 });
+
+// https://stackoverflow.com/questions/22207377/disable-click-outside-of-bootstrap-modal-area-to-close-modal
+// https://stackoverflow.com/questions/40211944/stop-playing-video-in-iframe-when-modal-is-closed
