3ce1a622272b99a78781e5cf6ee6bde242eb21ae
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-24 11:28:52 -0800

    1

---
+++ javascript/indexTranslations.js
@@ -0,0 +1,38 @@
+$(function(){
+    $("body").on('hidden.bs.modal', function (e) {
+      var $iframes = $(e.target).find("iframe");
+      $iframes.each(function(index, iframe){
+        $(iframe).attr("src", $(iframe).attr("src"));
+      });
+    });
+  });  
+$(function() {
+  "use strict";
+
+  let bodyA = $(".bodya");
+  let footer = $(".footer");
+  let main = $(".main");
+  let aside = $("aside");
+  let subheadercenterheadertextbutton = $(".subheadercenterheadertextbutton");
+  let header = $(".header");
+  let contentouter = $(".contentouter");
+  let modeToggle = $("#mode-toggle");
+  let modeStatus = $(".mode-status");
+
+  function toggleMode() {
+    bodyA.toggleClass("dark-modefields");
+    footer.toggleClass("dark-modefields");
+    main.toggleClass("dark-modefields");
+    aside.toggleClass("dark-modefields");
+    subheadercenterheadertextbutton.toggleClass("dark-modefields");
+    header.toggleClass("dark-modefields");
+    contentouter.toggleClass("dark-mode");
+
+    const modeMessage = contentouter.hasClass("dark-mode") ?
+      "Grayscale" : "Color";
+
+    modeStatus.text(modeMessage);
+  }
+
+  modeToggle.click(toggleMode);
+});
