a42be9395dbfdd335ed9c26ec1a6f797dbb9b180
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-21 04:20:35 -0800

    1

--- src/edu/unlv/mis768/kly/CreateTranscript.java
+++ src/edu/unlv/mis768/kly/CreateTranscript.java
@@ -124,6 +124,33 @@ public class CreateTranscript {
 
 
 
+
+
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ /* 
+
+> git push origin main:main
+error: RPC failed; HTTP 400 curl 92 HTTP/2 stream 7 was not closed cleanly: CANCEL (err 8)
+send-pack: unexpected disconnect while reading sideband packet
+fatal: the remote end hung up unexpectedly
+Everything up-to-date
+
+> git push origin main:main
+error: RPC failed; HTTP 400 curl 92 HTTP/2 stream 7 was not closed cleanly: CANCEL (err 8)
+send-pack: unexpected disconnect while reading sideband packet
+fatal: the remote end hung up unexpectedly
+Everything up-to-date
+
+ */
+
+
 //keyboard.nextLine();
 //}  while (yesOrNoEntry == 'y' || yesOrNoEntry == 'Y') {
 //if (yesOrNoEntry == 'y' || yesOrNoEntry == 'Y') {
