5458bdc7d5f97ee9ddc4a9ca5d74abc1fbbb2fc9
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-04-03 09:15:14 -0700

    221

---
+++ misc/04032024 0829/MIS768/src/MIS768/edu/unlv/mis768/kly/individualassignment4/DogStory.java
@@ -0,0 +1,83 @@
+package edu.unlv.mis768.kly.individualassignment4;
+
+import java.io.IOException;
+
+public class DogStory {
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("The Dog Story by Kenneth Larot Yamat dba Chestnut + Hazel (2024) "); System.out.println(" ");
+		
+		System.out.println("There once was a dog named");
+		System.out.println("Fellini the dog, and he was");
+		System.out.println("Trying to make a movie about");
+		System.out.println("how hard it really is to be a dog.");System.out.println(" ");
+		
+		System.out.println("there are so many things that a dog, like");
+		System.out.println("Fellini the Dog is responsible for.");
+		System.out.println("I have to eat dog food all day");System.out.println(" ");
+		
+		System.out.println("when I was a dog, said Fellini");
+		System.out.println("the days would really never end, NEVER!");
+		System.out.println("but you're still a dog, said Godard the Goose");System.out.println(" ");
+		
+		System.out.println("Is it then true now that");
+		System.out.println("your days still never end?");System.out.println(" ");
+		
+		System.out.println("It's true, affirmed Fellini the Dog");
+		System.out.println("to Godard the Goose, my days still never end!");System.out.println(" ");
+		
+//		System.out.println("when the chicken");
+//		System.out.println("first crossed the road");
+//		System.out.println("the chicken thought "); System.out.println("in it's avian brain");System.out.println(" ");
+//
+//		System.out.println("i like this road.");
+//		System.out.println("this is a nice road");
+//		System.out.println("and it is a road that");System.out.println(" ");
+//		
+//		System.out.println("does not appear to be well ");
+//		System.out.println("traveled");System.out.println("and yet still seems so well");System.out.println("maintained");
+//		System.out.println("and it does not");
+//		System.out.println("appear to have very much");System.out.println(" ");
+//		
+//		System.out.println("traffic   ");
+//		System.out.println("  and so  ");
+//		System.out.println("  the chicken ");
+//		
+//		System.out.println("crossed the road in an");
+//		System.out.println("attempt to get to the other side");System.out.println(" ");
+//	
+//		System.out.println("it went well for the chicken ");
+//		System.out.println("and the chicken was able to find ");
+//		System.out.println("whatever it is chickens eat.");System.out.println(" ");
+//		
+//		System.out.println("chicken food ");
+//		System.out.println("perhaps");
+//		System.out.println("or maybe duck food.");System.out.println(" ");
+//		
+//		System.out.println("whatever it was");
+//		System.out.println("i don't really know.");
+//		System.out.println("i wasn't actually there."); System.out.println(" ");
+//		
+//		System.out.println("but this is an old story that many ");
+//		System.out.println("people have told and retold ");
+//		System.out.println("throughout the generations ");
+//		System.out.println("so remember it "); System.out.println(" ");
+//		
+//		System.out.println("and yet there is one aspect ");
+//		System.out.println("of the story that proves ");
+//		System.out.println("to be a major oversight on the part ");
+//		System.out.println("of so many scholars of the chicken story ");System.out.println(" ");
+//		
+//		System.out.println("the chicken ");
+//		System.out.println("encountered ");
+//		System.out.println("a frog ");
+//		System.out.println("on the other side of the road ");
+//		System.out.println(" ");
+//		
+//		System.out.println("the frog, named: frog. ur. ");
+//		System.out.println("was trying to cross the road ");
+//		System.out.println("to get to the side  ");
+//		System.out.println("of the road the chicken was previously on ");
+
+}
+}
