8faf865d81847a4ea37c45a7ad1f9cb7041041ae
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-04-03 08:51:12 -0700

    220

---
+++ misc/04032024 0829/MIS768/src/2EmpoyeeandEmployeeDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * 
+ * I don't know what is happening here I think that this was supposed to be
+ * and assignment for my MIS 768 class
+ * but I don't think that I copied the right 
+ * 
+ * there is something strange
+ * where
+ * 
+ * it looks like a folder, but it's not really
+ * a folder or anything.
+ * some folders must have
+ * some kind of signature of being either
+ * a java package
+ * or a git repository, but
+ * they behave differently from other
+ * 
+ * kinds of folders.
+ */
