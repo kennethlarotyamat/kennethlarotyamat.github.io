877a6078ae95d429bcfa6d2255004dfde1fd60ce
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-04-05 01:35:05 -0700

    234

--- src/MIS768/src/dba/chestnutandhazel/SP/CottageCheese.java
+++ src/MIS768/src/dba/chestnutandhazel/SP/CottageCheese.java
@@ -4,6 +4,11 @@ public class CottageCheese {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("The Cottage Cheese Poem in Java #2 by Kenneth Larot Yamat dba Chestnut + Hazel (2024) "); System.out.println(" ");
+		
+		System.out.println("I was thinking about food and how cheese");
+		System.out.println("can become cottage cheese if left inside");
+		System.out.println("a cottage");
 
 	}
 
