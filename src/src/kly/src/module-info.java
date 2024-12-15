ebb541a1107a71548bcf662d9ed4bb70a64b5f91
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-12-15 00:01:05 -0800

    1

--- src/src/kly/src/module-info.java
+++ src/src/kly/src/module-info.java
@@ -1,8 +1,20 @@
-/**
- * 
- */
-/**
- * 
- */
-module chestnutandhazel {
-}
+// // /**
+// //  * 
+// //  */
+// // /**
+// //  * 
+// //  */
+// // module chestnutandhazel {
+// // }
+
+// /*
+//  * 
+//  * 
+//  * 
+//  * well, let's see where this
+//  * goes/ I basically commented out nearly everything
+//  * 
+//  * in all of these java files, and 
+//  * this may have fixed things or most htings or most things or 
+//  * most of the problems I was dealing with before.
+//  */
