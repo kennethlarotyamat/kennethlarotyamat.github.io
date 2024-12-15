c5699c99532885e7041dd42e3da0226bd9c2915f
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-12-14 22:16:22 -0800

    1

--- src/MIS768/src/module-info.java
+++ src/MIS768/src/module-info.java
@@ -2,7 +2,11 @@
  * 
  */
 /**
- * 
+ * but are there new things 
+ * that change over time and make
+ * those things that do things that
+ * can be things that you don't really know 
+ * and you see theere are things.
  */
 module dba.chestnutandhazel.SP {
 }
