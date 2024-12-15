36100f1816f537ad08ca440b46eb1ba9d6bb4511
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-12-14 23:57:31 -0800

    1

--- misc/04032024 0829/MIS768/src/MIS768/edu/unlv/mis768/labwork10/SportsCar.java
+++ misc/04032024 0829/MIS768/src/MIS768/edu/unlv/mis768/labwork10/SportsCar.java
@@ -1,73 +1,73 @@
-package edu.unlv.mis768.labwork10;
-import java.text.DecimalFormat;
+// package edu.unlv.mis768.labwork10;
+// import java.text.DecimalFormat;
 
-/**
-   SportsCar class
-*/
+// /**
+//    SportsCar class
+// */
 
-public class SportsCar {
-   private CarType make;    // The car's make
-   private CarColor color;  // The car's color
-   private double price;    // The car's price
+// public class SportsCar {
+//    private CarType make;    // The car's make
+//    private CarColor color;  // The car's color
+//    private double price;    // The car's price
    
-   /**
-      The constructor initializes the car's make, 
-      color, and price.
-      @param aMake The car's make.
-      @param aColor The car's color.
-      @param aPrice The car's price.
-   */
+//    /**
+//       The constructor initializes the car's make, 
+//       color, and price.
+//       @param aMake The car's make.
+//       @param aColor The car's color.
+//       @param aPrice The car's price.
+//    */
    
-   public SportsCar(CarType aMake, CarColor aColor, double aPrice) {
-      make = aMake;
-      color = aColor;
-      price = aPrice;
-   }
+//    public SportsCar(CarType aMake, CarColor aColor, double aPrice) {
+//       make = aMake;
+//       color = aColor;
+//       price = aPrice;
+//    }
 
-   /**
-      getMake method
-      @return The car's make.
-   */
+//    /**
+//       getMake method
+//       @return The car's make.
+//    */
    
-   public CarType getMake() {
-      return make;
-   }
+//    public CarType getMake() {
+//       return make;
+//    }
 
-   /**
-      getColor method
-      @return The car's color.
-   */
+//    /**
+//       getColor method
+//       @return The car's color.
+//    */
    
-   public CarColor getColor() {
-      return color;
-   }
+//    public CarColor getColor() {
+//       return color;
+//    }
    
-   /**
-      getPrice method
-      @return The car's price.
-   */
+//    /**
+//       getPrice method
+//       @return The car's price.
+//    */
    
-   public double getPrice() {
-      return price;
-   }
+//    public double getPrice() {
+//       return price;
+//    }
    
-   /**
-      toString method
-      @return A string indicating the car's make,
-              color, and price.
-   */
+//    /**
+//       toString method
+//       @return A string indicating the car's make,
+//               color, and price.
+//    */
    
-   public String toString() {
-      // Create a DecimalFormat object for
-      // dollar formatting.
-      DecimalFormat dollar = new DecimalFormat("#,##0.00");
+//    public String toString() {
+//       // Create a DecimalFormat object for
+//       // dollar formatting.
+//       DecimalFormat dollar = new DecimalFormat("#,##0.00");
       
-      // Create a string representing the object.
-      String str = "Make: " + make + 
-                   "\nColor: " + color +
-                   "\nPrice: $" + dollar.format(price);
+//       // Create a string representing the object.
+//       String str = "Make: " + make + 
+//                    "\nColor: " + color +
+//                    "\nPrice: $" + dollar.format(price);
       
-      // Return the string.
-      return str;
-   }
-}
+//       // Return the string.
+//       return str;
+//    }
+// }
