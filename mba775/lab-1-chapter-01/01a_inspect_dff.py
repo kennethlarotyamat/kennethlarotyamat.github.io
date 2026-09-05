13fb2e0303f5c58dad6ce4d3da3e24895a36b671
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2026-09-05 14:29:52 -0700

    1

--- mba775/lab-1-chapter-01/01a_inspect_dff.py
+++ mba775/lab-1-chapter-01/01a_inspect_dff.py
@@ -86,13 +86,13 @@ if n_duplicates:
     # what is the keyboard shortcut for block comments in a .py file?
     # so I ran the program after installing pandas, and this is what was produced:  
 
-    # 
-#     PS D:\GitHub> & C:\Python312\python.exe d:/GitHub/kennethlarotyamat.github.io/mba775/lab-1-chapter-01/01a_inspect_dff.py
+# 
+# PS D:\GitHub> & C:\Python312\python.exe d:/GitHub/kennethlarotyamat.github.io/mba775/lab-1-chapter-01/01a_inspect_dff.py
 # coming back around to this. this shouldn't really take too long, but for some reason I'm having trouble getting around to it. 9.5.2026 1.56 PM
 # distractions, over and over and over. It's kind of the way it goes. I'm figuring out
 # which version of "In a Sentimental Mood to download." I still buy digital music. Sorry.
 # I'm just trying to see if there is a version I like better than Duke Ellington and John Coltrane.
-#  I won't say that I'm trying to find a "better" version, just one that I like more.
+# I won't say that I'm trying to find a "better" version, just one that I like more.
 # there is alway some kind of distraction.
 
 
