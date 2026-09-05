87fbf303e042b0849ef9a08b17fa99ba4079b1c9
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2026-09-05 14:09:42 -0700

    1

--- mba775/lab-1-chapter-01/01a_inspect_dff.py
+++ mba775/lab-1-chapter-01/01a_inspect_dff.py
@@ -89,6 +89,8 @@ if n_duplicates:
     # 
 #     PS D:\GitHub> & C:\Python312\python.exe d:/GitHub/kennethlarotyamat.github.io/mba775/lab-1-chapter-01/01a_inspect_dff.py
 # coming back around to this. this shouldn't really take too long, but for some reason I'm having trouble getting around to it. 9.5.2026 1.56 PM
+# distractions, over and over and over. It's kind of the way it goes. I'm figuring out
+# which version of "In a Sentimental Mood to download." I still buy digital music. Sorry.
 # 1. What did we get?
 # -------------------
 #                   full history  since 2020-01-01
